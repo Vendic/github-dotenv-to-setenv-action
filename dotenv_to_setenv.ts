81812448c86cca3bf8c71c76dc0ea1299705855e
Tjitse Efdé <tjitse@vendic.nl>
2021-08-20 14:49:28 +0200

    Fix path

--- dotenv_to_setenv.ts
+++ dotenv_to_setenv.ts
@@ -1,12 +1,26 @@
 import * as core from '@actions/core'
 import * as fs from "fs";
 import * as dotenv from 'dotenv'
+import * as os from "os";
+import * as path from "path";
+
+function getAbsolutePath(inputFilePath: string): string {
+    if (inputFilePath[0] !== "~")
+        return path.resolve(inputFilePath);
+
+    const homeDirectory = os.homedir();
+    if (homeDirectory)
+        return path.join(homeDirectory, inputFilePath.slice(1));
+
+    throw new Error("Unable to resole `~` to HOME");
+}
 
 const run = async (): Promise<void> => {
     try {
         core.debug('Starting reading .env id extraction.')
+        const path = getAbsolutePath(core.getInput("path", {required: true}));
+        core.debug("Path : " + path);
 
-        const path = core.getInput('path')
         if (fs.existsSync(path) == false) {
             core.setFailed(`No .env file found on path ${path}`)
             return;
